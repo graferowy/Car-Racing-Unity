ad1c9b8f4dd818cfb624d1957cb711776bb6a394
Fabryka Twórców Gier <filip@kaszczynski.pl>
2021-01-06 20:59:12 +0100

    #3 Basic Car Movement
    3. Player Car Movement Implementation

---
+++ Assets/Scripts/PlayerCarMovement.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarMovement : MonoBehaviour
+{
+    public float carHorizontalSpeed = 2f;
+    private Vector3 carPosition;
+
+    private void Start()
+    {
+        carPosition = this.gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        carPosition.x += Input.GetAxis("Horizontal") * carHorizontalSpeed * Time.deltaTime;
+        carPosition.x = Mathf.Clamp(carPosition.x, -2.41f, 2.41f);
+        this.gameObject.transform.position = carPosition;
+    }
+}
