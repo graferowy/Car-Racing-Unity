33eab8b408ddf69fad41e85d0745257912e8983c
Fabryka Twórców Gier <filip@kaszczynski.pl>
2021-01-06 22:26:57 +0100

    #8 Bandit Cars
    8. Bandit cars behaviour

---
+++ Assets/Scripts/BanditCarBehaviour.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BanditCarBehaviour : MonoBehaviour
+{
+    public GameObject bomb;
+    public int bombsAmount;
+    public int banditCarVerticalSpeed;
+    public int banditCarHorizontalSpeed;
+    public float bombDelay;
+
+    private float Delay;
+    private GameObject playerCar;
+    private Vector3 banditCarPos;
+
+    private void Start()
+    {
+        playerCar = GameObject.FindWithTag("Player");
+        Delay = bombDelay;
+    }
+
+    private void Update()
+    {
+        if (playerCar == null)
+        {
+            playerCar = GameObject.FindWithTag("Player");
+        }
+        else
+        {
+            if (gameObject.transform.position.y > 3.8f && bombsAmount > 0)
+            {
+                this.gameObject.transform.Translate(new Vector3(0, -1, 0) * banditCarVerticalSpeed * Time.deltaTime);
+            }
+            else if (bombsAmount <= 0)
+            {
+                this.gameObject.transform.Translate(new Vector3(0, 1, 0) * banditCarVerticalSpeed * Time.deltaTime);
+
+                if (gameObject.transform.position.y > 6.5f)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+            else
+            {
+                banditCarPos = Vector3.Lerp(transform.position, playerCar.transform.position, Time.deltaTime * banditCarHorizontalSpeed);
+                transform.position = new Vector3(banditCarPos.x, transform.position.y, 0);
+
+                Delay -= Time.deltaTime;
+
+                if (Delay <= 0 && bombsAmount > 0)
+                {
+                    Delay = bombDelay;
+                    bombsAmount--;
+                    Instantiate(bomb, transform.position, Quaternion.identity);
+                }
+                else if (Delay <= 0 && bombsAmount <= 5 && bombsAmount > 0)
+                {
+                    Delay = bombDelay / 2;
+                    bombsAmount--;
+                    Instantiate(bomb, transform.position, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public int bombDamage;
+    public float bombSpeed;
+
+    private void Update()
+    {
+        this.gameObject.transform.Translate(new Vector3(0, -1, 0) * bombSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.gameObject.tag == "Player")
+        {
+            obj.gameObject.GetComponent<PlayerCarMovement>().durability -= bombDamage;
+            Destroy(this.gameObject);
+        } 
+        else if (obj.gameObject.tag == "Shield")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (obj.gameObject.tag == "EndOfTheRoad")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
