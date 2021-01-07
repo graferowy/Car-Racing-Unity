28e81e54f885db7b189d86a6dfb9763f9252e9c2
Fabryka Twórców Gier <filip@kaszczynski.pl>
2021-01-07 10:05:58 +0100

    #15 Game Pause
    15. Game Pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseObj;
+    private float tempTimeScale;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale != 0)
+            {
+                tempTimeScale = Time.timeScale;
+            }
+
+            PauseGame();
+        }
+    }
+
+    private void PauseGame()
+    {
+        pauseObj.SetActive(!pauseObj.activeInHierarchy);
+
+        if (Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = tempTimeScale;
+        }
+    }
+
+    public void ResumeButton()
+    {
+        PauseGame();
+    }
+
+    public void MenuExitButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
